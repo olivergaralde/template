be9b7249542a72ca40e4a85b6d0dcbd51cde8fa7
oliver-sys <jamesoliverg@outlook.com>
2026-09-24 22:26:24 +0800

    Set up base template

---
+++ src/config/site.ts
@@ -0,0 +1,6 @@
+export const siteConfig = {
+  name: "Template",
+  description:
+    "A reusable Next.js starter: TypeScript, Tailwind, shadcn/ui-style components.",
+  url: process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
+};
